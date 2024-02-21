3cf4eb2404635398b9f26688ed8f1a505df294d8
vuzaldo <121903720+vuzaldo@users.noreply.github.com>
2024-02-20 23:39:04 -0300

    Fix highlighted skill issues (repeated skills/different targeting)

--- scripts/cards_gui.js
+++ scripts/cards_gui.js
@@ -243,10 +243,11 @@ var CARD_GUI = {};
             runeIDs.push(runes[i].id);
             var rune = getRune(runeID);
             for (var key in rune.stat_boost) {
+                boosts[key] = true;
                 if (key == "skill") {
                     key = rune.stat_boost.skill.id;
+                    boosts[key] = { all: rune.stat_boost.skill.all, used: false };
                 }
-                boosts[key] = true;
             }
         }
         var highlighted = card.highlighted;
@@ -408,17 +409,18 @@ var CARD_GUI = {};
     function getSkillsHtml(card, divSkills, skillsShort, skills, onField, boosts) {
         for (var i = 0; i < skills.length; i++) {
             var origSkill = skills[i];
+            var boost = boosts[origSkill.id];
             var skill = {
                 all: origSkill.all,
-                boosted: origSkill.boosted,
                 c: origSkill.c,
                 countdown: origSkill.countdown,
                 id: origSkill.id,
                 s: origSkill.s,
                 x: origSkill.x,
                 y: origSkill.y,
-                boosted: boosts[origSkill.id]
+                boosted: boost && !boost.used && (boost.all == origSkill.all)
             };
+            boost && (boost.used = skill.boosted);
             divSkills.appendChild(getSkillHtml(card, skill, onField, i));
             divSkills.appendChild(document.createElement('br'));
             skillsShort.appendChild(getSkillIcon(skill.id));
