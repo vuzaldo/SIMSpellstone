a17879a354ae1f2970b7f8f51527984807a1e414
vuzaldo <121903720+vuzaldo@users.noreply.github.com>
2025-06-21 22:40:49 -0300

    Scale stats before scaling attributes (map node)

--- scripts/shared.js
+++ scripts/shared.js
@@ -697,6 +697,12 @@ var makeUnit = (function() {
                 if (b.modifierType === 'scale_attributes') return 1;
                 return 0;
             });
+            // Scale stats before scaling attributes
+            skillModifiers.sort(function(a, b) { // move to the start of the array
+                if (a.modifierType === 'scale_stat') return -1;
+                if (b.modifierType === 'scale_stat') return 1;
+                return 0;
+            });
 
             modifySkillsPostRune(card, original_skills, skillModifiers, isToken);
         }
@@ -735,7 +741,7 @@ var getSkillMult = function(skill, target, defaultBase) {
     var mult = skill.mult;
     if (mult) {
         var base = skill.base || defaultBase || 'health';
-        base = base == 'health' ? 'base_health': base;
+        base = base == 'health' ? 'base_health' : base;
         return Math.ceil(mult * target[base]);
     } else {
         return 0;
